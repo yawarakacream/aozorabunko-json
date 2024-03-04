28e2346c06449c5080987cc306c58d5d154117b4
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2024-03-04 23:44:44 +0900

    Support ruby, gaiji

--- src/book_file_parser.rs
+++ src/book_file_parser.rs
@@ -1,20 +1,24 @@
-use anyhow::{Context, Ok, Result};
+use anyhow::{bail, ensure, Context, Ok, Result};
 use once_cell::sync::Lazy;
 use regex::Regex;
 use serde::Serialize;
 
-use crate::utility::trim_empty_lines;
+use crate::{
+    accent_composer::compose_accent,
+    jis_x_0213,
+    utility::{trim_empty_lines, CharType},
+};
 
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "kebab-case")]
-pub enum BookContentOriginalType {
+pub enum BookContentOriginalDataType {
     RubyTxt,
 }
 
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BookContent {
-    pub original_type: BookContentOriginalType,
+    pub original_data_type: BookContentOriginalDataType,
     pub header: Vec<String>,
     pub body: Vec<Vec<BookLine>>,
     pub footer: Vec<String>,
@@ -26,7 +30,7 @@ pub struct BookLine {
 }
 
 #[derive(Debug, Serialize)]
-#[serde(rename_all = "camelCase", tag = "type")]
+#[serde(rename_all = "kebab-case", tag = "type")]
 pub enum BookLineContent {
     String { value: String, ruby: Option<String> },
     Annotation {},
@@ -67,27 +71,39 @@ pub fn parse_ruby_txt(txt: &str) -> Result<BookContent> {
         header
     };
 
+    // 冒頭から本文の間の空白行を飛ばす
+    while txt.peek().context("Body is missing")?.is_empty() {
+        txt.next();
+    }
+
     let body = {
-        // 区切りのハイフンの個数は一定でない
+        // 主に "【テキスト中に現れる記号について】" を表す区切り
+        // その他にも単なる（ページの？）区切りとして使われることもある
+        // 個数は一定でない
         static REGEX_ALL_HYPHEN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^\-+$").unwrap());
 
         // 基本は "底本："
         static REGEX_FOOTER_CHECKER: Lazy<Regex> =
-            Lazy::new(|| Regex::new(r"^底本(・初出)?(：|:)").unwrap());
+            Lazy::new(|| Regex::new(r"^底本()?(：|:)").unwrap());
+
+        static REGEX_ANNOTATION_DESCRIPTION: Lazy<Regex> = Lazy::new(|| {
+            Regex::new(r"^(\s|　)*(【|《)テキス.中に現れる記号について(】|》)(\s|　)*$").unwrap()
+        });
 
         let mut body = vec![vec![]];
 
-        loop {
-            let &line = txt.peek().context("Text ends without footer")?;
-
+        while let Some(&line) = txt.peek() {
             if REGEX_FOOTER_CHECKER.is_match(line) {
+                break;
+            }
+
+            if line == "［＃本文終わり］" {
+                txt.next();
                 break;
             }
 
             txt.next();
 
-            // 主に "【テキスト中に現れる記号について】" への対応
-            // その他にも単なる（ページの？）区切りとして使われることもある
             if REGEX_ALL_HYPHEN.is_match(line) {
                 if !body.last().unwrap().is_empty() {
                     body.push(vec![]);
@@ -97,22 +113,43 @@ pub fn parse_ruby_txt(txt: &str) -> Result<BookContent> {
             }
         }
 
-        let body = body
-            .into_iter()
-            .map(|mut block| {
-                trim_empty_lines(&mut block);
+        // 長ハイフン (REGEX_ALL_HYPHEN) を footer の区切りにしているものがある
+        if body.last().unwrap().is_empty() {
+            body.pop();
+        }
 
-                block
-                    .into_iter()
-                    .map(|line| BookLine {
-                        contents: vec![BookLineContent::String {
-                            value: line.to_string(),
-                            ruby: None,
-                        }],
-                    })
-                    .collect()
-            })
-            .collect();
+        let body = {
+            let mut new_body = Vec::with_capacity(body.len());
+
+            for block in &mut body {
+                trim_empty_lines(block);
+
+                ensure!(!block.is_empty());
+
+                if REGEX_ANNOTATION_DESCRIPTION.is_match(block[0]) {
+                    let mut new_block = Vec::with_capacity(block.len());
+                    for line in block {
+                        new_block.push(BookLine {
+                            contents: vec![BookLineContent::String {
+                                value: line.to_string(),
+                                ruby: None,
+                            }],
+                        });
+                    }
+                    new_body.push(new_block);
+                    continue;
+                }
+
+                let mut new_block = Vec::new();
+                for line in block {
+                    let line = parse_line(line)
+                        .with_context(|| format!("Failed to parse line {:?}", line))?;
+                    new_block.push(line);
+                }
+                new_body.push(new_block);
+            }
+            new_body
+        };
 
         body
     };
@@ -128,9 +165,175 @@ pub fn parse_ruby_txt(txt: &str) -> Result<BookContent> {
     };
 
     Ok(BookContent {
-        original_type: BookContentOriginalType::RubyTxt,
+        original_data_type: BookContentOriginalDataType::RubyTxt,
         header,
         body,
         footer,
     })
+}
+
+fn parse_line(line: &str) -> Result<BookLine> {
+    if line.is_empty() {
+        return Ok(BookLine {
+            contents: Vec::new(),
+        });
+    }
+
+    // 外字（第 1, 2 水準にない漢字）
+    let line = {
+        static REGEX_GAIJI: Lazy<Regex> = Lazy::new(|| {
+            Regex::new(
+                r"※［＃「.+?」、第[0-9]水準(?P<plane>[0-9]+)-(?P<row>[0-9]+)-(?P<cell>[0-9]+)］?",
+            )
+            .unwrap()
+        });
+
+        REGEX_GAIJI
+            .replace_all(line, |caps: &regex::Captures<'_>| {
+                let plane = caps.name("plane").unwrap().as_str().parse().unwrap();
+                let row = caps.name("row").unwrap().as_str().parse().unwrap();
+                let cell = caps.name("cell").unwrap().as_str().parse().unwrap();
+                jis_x_0213::JIS_X_0213.get(&(plane, row, cell)).unwrap()
+            })
+            .into_owned()
+    };
+
+    // 外字（アクセント分解）
+    // アクセント分解したものにルビが振られていることがあるので先に処理する
+    let line = {
+        let mut chars = line.chars().into_iter().peekable();
+
+        let mut line = String::new();
+
+        while let Some(c) = chars.next() {
+            match c {
+                '〔' => {
+                    let mut s0 = String::new();
+                    let mut try_compose = false;
+                    while let Some(c) = chars.next() {
+                        if c == '〕' {
+                            try_compose = true;
+                            break;
+                        }
+                        s0.push(c);
+                    }
+
+                    // '〕' なしで行が終わったときはそのまま
+                    if !try_compose {
+                        line.push('〔');
+                        line.push_str(&s0);
+                        break;
+                    }
+
+                    let s1 = compose_accent(&s0);
+
+                    // 分解結果が変わらなければ括弧も含めて戻す
+                    if s0 == s1 {
+                        line.push('〔');
+                        line.push_str(&s0);
+                        line.push('〕');
+                    } else {
+                        line.push_str(&s1);
+                    }
+                }
+                _ => line.push(c),
+            }
+        }
+
+        line
+    };
+
+    let mut contents = Vec::new();
+
+    let mut chars = line.chars().into_iter().peekable();
+    while let Some(c) = chars.next() {
+        match (c, chars.peek()) {
+            // ルビ
+            ('《', _) => {
+                let mut ruby = String::new();
+                loop {
+                    let c = chars.next().context("A line ends without '》'")?;
+                    if c == '》' {
+                        break;
+                    }
+                    ruby.push(c);
+                }
+
+                match contents.pop().context("No element found to add ruby")? {
+                    BookLineContent::String { value, ruby: ruby0 } => {
+                        ensure!(!value.is_empty(), "Cannot add ruby to empty String");
+                        ensure!(ruby0.is_none(), "Cannot add 2 rubies to 1 String");
+
+                        let mut chars: Vec<_> = value.chars().collect();
+
+                        let ruby_start_index = {
+                            let mut ruby_start_char_index = None;
+                            for (i, c) in chars.iter().enumerate().rev() {
+                                if c == &'｜' {
+                                    ensure!(
+                                        ruby_start_char_index.is_none(),
+                                        "Duplicate '｜' are found at {} and {} in {:?}",
+                                        ruby_start_char_index.unwrap(),
+                                        i,
+                                        value
+                                    );
+                                    ruby_start_char_index = Some(i);
+                                }
+                            }
+
+                            if let Some(ruby_start_char_index) = ruby_start_char_index {
+                                ensure!(chars.remove(ruby_start_char_index) == '｜');
+                                ruby_start_char_index
+                            } else {
+                                let mut ruby_start_index = chars.len();
+                                let last_char_type = CharType::from(*chars.last().unwrap());
+                                for c in chars.iter().rev() {
+                                    if CharType::from(*c) != last_char_type {
+                                        break;
+                                    }
+                                    ruby_start_index -= 1;
+                                }
+                                ruby_start_index
+                            }
+                        };
+
+                        if 0 < ruby_start_index {
+                            contents.push(BookLineContent::String {
+                                value: chars[..ruby_start_index].iter().collect(),
+                                ruby: None,
+                            });
+                        }
+                        contents.push(BookLineContent::String {
+                            value: chars[ruby_start_index..].iter().collect(),
+                            ruby: Some(ruby),
+                        });
+                    }
+                    _ => {
+                        bail!("Cannot add character to non-String content")
+                    }
+                }
+            }
+
+            _ => match contents.last_mut() {
+                Some(BookLineContent::String { value, ruby: None }) => value.push(c),
+                _ => contents.push(BookLineContent::String {
+                    value: c.to_string(),
+                    ruby: None,
+                }),
+            },
+        }
+    }
+
+    // validate
+    match &contents.last() {
+        Some(last) => match last {
+            BookLineContent::String { value, ruby: _ } => {
+                ensure!(!value.is_empty(), "last of contents is empty");
+            }
+            _ => {}
+        },
+        None => {}
+    }
+
+    Ok(BookLine { contents })
 }
